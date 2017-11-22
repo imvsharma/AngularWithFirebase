0103049d73b65de6cb67c47fe90a443bbc36520b
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2017-11-22 07:49:38 +0530

    Home page redesign

--- Angular Authentication with Firebase/src/app/app.module.ts
+++ Angular Authentication with Firebase/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -10,12 +9,15 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AuthService } from './auth.service';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- Angular Authentication with Firebase/src/app/app.routes.ts
+++ Angular Authentication with Firebase/src/app/app.routes.ts
@@ -2,24 +2,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import { HomeComponent } from './home/home.component';
 const routes : Routes = [
+    // {
+    //     path : '',
+    //     redirectTo : '/',
+    //     pathMatch : 'full'
+    // }, 
     {
         path : '',
-        redirectTo : '/',
-        pathMatch : 'full'
-    }, 
-    {
-        path : '',
-        component : LoginComponent
-    },
-    {
-        path : 'signup',
-        component: SignupComponent
-    },
-    {
-        path : 'profile',
-        component: ProfileComponent
+        component : HomeComponent,
+        children : [
+            {
+                path : '',
+                component : LoginComponent
+            },
+            {
+                path : 'signup',
+                component: SignupComponent
+            },
+        ]
     }
 ];
 
